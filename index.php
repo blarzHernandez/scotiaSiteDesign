f6cbc3e7968f486242989e7b665431f9fd9e1468
Roberto Hernandez <roberto.hernandez@gruporaf.com>
2017-08-29 17:54:09 -0600

    slide

--- index.php
+++ index.php
@@ -32,9 +32,16 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
     		</button>
-    		<a class="navbar-brand" href="#">
-					Scotia Bank
-				</a>
+
+        <a href="http://www.creative-tim.com">
+	        	<div class="logo-container">
+	                <div class="logo">
+	                    <img src="assets/img/logo.png" alt="Home" rel="tooltip" title="Scotia Bank" data-placement="bottom" data-html="true">
+	                </div>
+
+
+				</div>
+	      	</a>
     	</div>
 
     	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -46,31 +53,41 @@
 									  <li><a href="#">Banca Para PYMES</a></li>
 							  		<li><a href="#">Mas</a></li>
 	        			</ul>
-								<li>
-									<a rel="tooltip" title="Siguenos en Twitter" data-placement="bottom" href="https://www.facebook.com/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
-										<i class="fa fa-twitter"></i>
-									</a>
-								</li>
-							</li>
 
-							<li>
+							</li>
+              <li>
+
+
+  		                <input type="text" class="form-control" placeholder="Buscar">
+
+
+
+
+              </li>
+              <li class="pull-right">
+                <a rel="tooltip" title="Siguenos en Twitter" data-placement="bottom" href="https://www.facebook.com/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
+                  <i class="fa fa-twitter"></i>
+                </a>
+              </li>
+
+							<li class="pull-right">
 								<a rel="tooltip" title="Siguenos en Facebook" data-placement="bottom" href="https://www.facebook.com/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
 									<i class="fa fa-facebook-square"></i>
 								</a>
 							</li>
-				<li>
+				<li class="pull-right">
 					<a rel="tooltip" title="Siguenos en Insta" data-placement="bottom" href="https://www.instagram.com/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
 						<i class="fa fa-instagram"></i>
 					</a>
 				</li>
 
-				<li>
+				<li class="pull-right">
 					<a rel="tooltip" title="Contactanos" data-placement="bottom" href="#Contacto" target="_blank" class="btn btn-white btn-simple btn-just-icon">
 						<i class="fa fa-phone"></i> (503) 2250-1111
 					</a>
 				</li>
-				<li>
-					<a href="https://www.scotiabank.com.sv/" target="_blank" class="pull-right" >
+				<li class="pull-left">
+					<a href="https://www.scotiabank.com.sv/" target="_blank"  >
 						<i class="fa fa-user-circle"></i> Ingreso
 					</a>
 				</li>
@@ -87,8 +104,39 @@
 
 <div class="wrapper">
 	<div class="header">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
 
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="assets/img/bg2.jpeg" alt="Test1" style="width:100%;">
+        </div>
+
+        <div class="item">
+          <img src="assets/img/bg3.jpeg" alt="Test2" style="width:100%;height:50%;">
+        </div>
+
+
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
 	</div>
+
 	<!-- you can use the class main-raised if you want the main area to be as a page with shadows -->
 	<div class="main">
 		<div class="container">
