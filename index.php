4e4969f9858b860d01ac6e7e9d410ee9a315ef8c
Blarz hdz <info.redpaz@gmail.com>
2017-08-29 23:56:50 -0600

    beta-01

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="assets/css/myStyles.css" />
 
 	<!-- CSS Files -->
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
@@ -22,89 +23,121 @@
 
 <body>
 
-<!-- Navbar will come here -->
-<nav class="navbar alert alert-info"  role="navigation">
-  	<div class="container-fluid">
-      <div class="s">
-        sadad
-      </div>
+<!-- Navbar top -->
+<nav class="social-nav default "  role="navigation">	
+  	<div class="container-fluid">		 
+       <ul class="nav navbar-nav navbar-left">
+              <li class="dropdown">
+	        			<a href="#" class="dropdown-toggle active" data-toggle="dropdown">	Banca Personal <b class="caret"></b></a>
+	        			<ul class="dropdown-menu">
+									  <li><a href="#">Banca Corporativa</a></li>
+									  <li><a href="#">Banca Para PYMES</a></li>
+							  		<li><a href="#">Mas</a></li>
+	        			</ul>
+							</li>
+      </ul>
+			
+					<form class="navbar-form navbar-left">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Buscar">
+								<a href="#"><i class="fa fa-search"></i> </a>
+							</div>
+							
+						<div class="input-group">
+										<span class="input-group-addon">
+											<i class="material-icons">country</i>
+										</span>
+										<select class="form-control" >
+											<option>El Salvador</option>
+									<option>El Salvador 2</option>
+											</select>
+									</div>
+						
+									
+						</form>
+				
+			<ul class="nav navbar-nav navbar-right">
+             <li>
+                <a rel="tooltip" title="Siguenos en Twitter" data-placement="bottom" href="https://www.facebook.com/" target="_blank" class="btn btn-black btn-simple btn-just-icon">
+                  <i class="fa fa-twitter"></i>
+                </a>
+              </li>
+              	<li>
+								<a rel="tooltip" title="Siguenos en Facebook" data-placement="bottom" href="https://www.facebook.com/" target="_blank" class="btn btn-black btn-simple btn-just-icon">
+									<i class="fa fa-facebook-square"></i>
+								</a>
+							</li>
+              <li>
+									<a rel="tooltip" title="Siguenos en Instagram" data-placement="bottom" href="https://www.instagram.com/" target="_blank" class="btn btn-black btn-simple btn-just-icon">
+									<i class="fa fa-instagram"></i>
+								</a>
+							</li>
+							<li>
+					<a rel="tooltip" title="Contactanos" data-placement="bottom" href="#Contacto" target="_blank" class="btn btn-black btn-simple btn-just-icon">
+						<i class="fa fa-phone"></i> (503) 2250-1111
+					</a>
+				</li>
+				<li >
+					<a href="#" data-toggle="modal" data-target="#myModal" target="_blank"  >
+						<i class="fa fa-user-circle"></i> Ingreso
+					</a>
+				</li>
+			</ul>
     </div>
 </nav>
+<!-- Navbar sewcond -->
 <nav class="navbar navbar-default" role="navigation">
 	<div class="container-fluid">
     	<div class="navbar-header">
+			
     		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
+                <span class="sr-only">Toggle</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
     		</button>
 
-        <a href="http://www.creative-tim.com">
+        <a href="http://www.scotiabank.com.sv">
 	        	<div class="logo-container">
 	                <div class="logo">
-	                    <img src="assets/img/logo.png" alt="Home" rel="tooltip" title="Scotia Bank" data-placement="bottom" data-html="true">
+	                   <img src="assets/img/logo.png" alt="Home" rel="tooltip" title="Scotia Bank" data-placement="bottom" data-html="true">
+										 
 	                </div>
-
-
-				</div>
+									
+					</div>
 	      	</a>
     	</div>
 
     	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     		<ul class="nav navbar-nav">
         		<li class="dropdown">
-	        			<a href="#" class="dropdown-toggle" data-toggle="dropdown">	Banca Personal <b class="caret"></b></a>
+	        			<a href="#" class="dropdown-toggle" data-toggle="dropdown">	Cuentas y Planes de Ahorro<i class="icon-ellipsis-vertical"></i> <b class="caret"></b></a>
 	        			<ul class="dropdown-menu">
-									  <li><a href="#">Banca Corporativa</a></li>
-									  <li><a href="#">Banca Para PYMES</a></li>
-							  		<li><a href="#">Mas</a></li>
+									  <li><a href="#">	<i class="fa fa-credit-card"></i> Cuenta de Ahorro</a></li>
+									  <li><a href="#">	<i class="fa fa-money"></i> Cuenta Corriente</a></li>
+							  		<li><a href="#">Otra</a></li>
 	        			</ul>
-
+							</li>   
+							<li class="dropdown">
+	        			<a href="#" class="dropdown-toggle" data-toggle="dropdown">	Tarjetas de Credito<i class="icon-ellipsis-vertical"></i> <b class="caret"></b></a>
+	        			<ul class="dropdown-menu">
+									  <li><a href="#">	<i class="fa fa-credit-card"></i> Cuenta de Ahorro</a></li>
+									  <li><a href="#">	<i class="fa fa-money"></i> Cuenta Corriente</a></li>
+							  		<li><a href="#">Otra</a></li>
+	        			</ul>
 							</li>
-              <li>
-
-
-  		                <input type="text" class="form-control" placeholder="Buscar">
-
-
-
-
-              </li>
-              <li class="pull-right">
-                <a rel="tooltip" title="Siguenos en Twitter" data-placement="bottom" href="https://www.facebook.com/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
-                  <i class="fa fa-twitter"></i>
-                </a>
-              </li>
-
-							<li class="pull-right">
-								<a rel="tooltip" title="Siguenos en Facebook" data-placement="bottom" href="https://www.facebook.com/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
-									<i class="fa fa-facebook-square"></i>
-								</a>
+							<li class="dropdown">
+	        			<a href="#" class="dropdown-toggle" data-toggle="dropdown">	Creditos<i class="icon-ellipsis-vertical"></i> <b class="caret"></b></a>
+	        			<ul class="dropdown-menu">
+									  <li><a href="#">	<i class="fa fa-user"></i> Personales</a></li>
+									  <li><a href="#">	<i class="fa fa-car"></i> Automotriz</a></li>
+							  		<li><a href="#">Otra</a></li>
+	        			</ul>
 							</li>
-				<li class="pull-right">
-					<a rel="tooltip" title="Siguenos en Insta" data-placement="bottom" href="https://www.instagram.com/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
-						<i class="fa fa-instagram"></i>
-					</a>
-				</li>
-
-				<li class="pull-right">
-					<a rel="tooltip" title="Contactanos" data-placement="bottom" href="#Contacto" target="_blank" class="btn btn-white btn-simple btn-just-icon">
-						<i class="fa fa-phone"></i> (503) 2250-1111
-					</a>
-				</li>
-				<li class="pull-left">
-					<a href="https://www.scotiabank.com.sv/" target="_blank"  >
-						<i class="fa fa-user-circle"></i> Ingreso
-					</a>
-				</li>
-
-
-
-
     		</ul>
     	</div>
 	</div>
+	<br>
 </nav>
 
 <!-- end navbar -->
@@ -126,7 +159,7 @@
         </div>
 
         <div class="item">
-          <img src="assets/1   /bg3.jpeg" alt="Test2" style="width:100%;height:50%;">
+          <img src="assets/img/bg3.jpeg" alt="Test2" style="width:100%;height:20% ;">
         </div>
 
 
@@ -149,9 +182,157 @@
 		<div class="container">
 
 			<!-- here you can add your content -->
+			<!-- Tabs with icons on Card -->
+						<div class="card card-nav-tabs">
+							<div class="header header-danger">
+								<!-- colors: "header-primary", "header-info", "header-success", "header-warning", "header-danger" -->
+								<div class="nav-tabs-navigation">
+									<div class="nav-tabs-wrapper">
+										<ul class="nav nav-tabs" data-tabs="tabs">
+											<li class="active">
+												<a href="#sucursales" data-toggle="tab">
+													<i class="material-icons">home</i>
+													Sucursales
+												</a>
+											</li>
+											<li>
+												<a href="#contactos" data-toggle="tab">
+													<i class="material-icons">chat</i>
+													Contactanos
+												</a>
+											</li>
+										
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="content">
+								<div class="tab-content text-center">
+									<div class="tab-pane active" id="sucursales">
+										<p> Sucursales y Cajeros</p>
+									</div>
+									<div class="tab-pane" id="contactos">
+									 <p> Contactanos</p>
+									</div>
+									
+								</div>
+							</div>
+						</div>
+						<!-- End Tabs with icons on Card -->
+			<div class = "row">
+				<hr>
+			<div class="card" style="width: 35rem;">
+  <img class="card-img-top" src="assets/img/bg3.jpeg" alt="Card image cap" style="width:318px;">
+  <div class="card-block">
+    <h4 class="card-title">Obten Credito PYME</h4>
+    <p class="card-text">¿Y si
+						ampliamos
+						nuestro
+						negocio?
+						Piensa en
+						grande
+						Obtén un crédito
+						PYME de
+						Scotiabank</p>
+    <a href="#" class="btn btn-primary">Mas Info..</a>
+  </div>
+</div>
+
+
+
+		<div class="card" style="width: 35rem;">
+  <img class="card-img-top" src="assets/img/bg2.jpeg" alt="Card image cap" style="width:318px;">
+  <div class="card-block">
+    <h4 class="card-title">Una Banca Con Proposito</h4>
+    <p class="card-text">Programa Comunitario de Scotiabank
+Comprometidos con la educación, la salud, el medio ambiente y la comunidad..</p>
+    <a href="#" class="btn btn-primary">Mas Info..</a>
+  </div>
+</div>
+
+		<div class="card" style="width: 35rem;">
+  <img class="card-img-top" src="assets/img/bg3.jpeg" alt="Card image cap" style="width:318px;">
+  <div class="card-block">
+    <h4 class="card-title">Obten Credito PYME</h4>
+    <p class="card-text">¿Y si
+						ampliamos
+						nuestro
+						negocio?
+						Piensa en
+						grande
+						Obtén un crédito
+						PYME de
+						Scotiabank</p>
+    <a href="#" class="btn btn-primary">Mas Info..</a>
+  </div>
+</div>
+
+	<div class="card" style="width: 35rem;">
+  <img class="card-img-top" src="assets/img/bg3.jpeg" alt="Card image cap" style="width:318px;">
+  <div class="card-block">
+    <h4 class="card-title">Obten Credito PYME</h4>
+    <p class="card-text">¿Y si
+						ampliamos
+						nuestro
+						negocio?
+						Piensa en
+						grande
+						Obtén un crédito
+						PYME de
+						Scotiabank</p>
+    <a href="#" class="btn btn-primary">Mas Info..</a>
+  </div>
+</div>
+	<div class="card" style="width: 35rem;">
+  <img class="card-img-top" src="assets/img/bg3.jpeg" alt="Card image cap" style="width:318px;">
+  <div class="card-block">
+    <h4 class="card-title">Obten Credito PYME</h4>
+    <p class="card-text">¿Y si
+						ampliamos
+						nuestro
+						negocio?
+						Piensa en
+						grande
+						Obtén un crédito
+						PYME de
+						Scotiabank</p>
+    <a href="#" class="btn btn-primary">Mas Info..</a>
+  </div>
+</div>
 
 		</div>
 	</div>
+	<footer class="footer">
+	    <div class="container">
+	        <nav class="pull-left">
+	            <ul>
+					<li>
+						<a href="#avisoLegal">
+							Aviso Legal
+						</a>
+					</li>
+					<li>
+						<a href="#privacidad">
+						Privacidad
+						</a>
+					</li>
+					<li>
+						<a href="#tasasYcomisiones">
+						   Tasas Y Comisiones
+						</a>
+					</li>
+					<li>
+						<a href="#modeloEscritura">
+							Modelos de Escritura
+						</a>
+					</li>
+	            </ul>
+	        </nav>
+	        <div class="copyright pull-right">
+	            &copy; 2017, by Blarz.
+	        </div>
+	    </div>
+	</footer>
 </div>
 
 
@@ -172,3 +353,64 @@
 	<script src="assets/js/material-kit.js" type="text/javascript"></script>
 
 </html>
+<!-- Modal Core -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel"></h4>
+      </div>
+      <div class="modal-body">
+     
+				<div class="row">
+					<div class="col-md-12">
+						<div class="card card-signup">
+							<form class="form" method="" action="">
+								<div class="header header-danger text-center">
+									<h4>Ingresar</h4>
+									<div class="social-line">
+										<a href="#cuenta_facebook" class="btn btn-simple btn-just-icon">
+											<i class="fa fa-facebook-square"></i>
+										</a>
+										<a href="#cuenta_twitter" class="btn btn-simple btn-just-icon">
+											<i class="fa fa-twitter"></i>
+										</a>
+										<a href="#cuenta_google" class="btn btn-simple btn-just-icon">
+											<i class="fa fa-google-plus"></i>
+										</a>
+									</div>
+								</div>
+								<p class="text-divider">O Con la Forma Tradicional</p>
+								<div class="content">
+
+								
+									<div class="input-group">
+										<span class="input-group-addon">
+											<i class="material-icons">email</i>
+										</span>
+										<input type="text" class="form-control" placeholder="Email...">
+									</div>
+
+									<div class="input-group">
+										<span class="input-group-addon">
+											<i class="material-icons">lock_outline</i>
+										</span>
+										<input type="password" placeholder="Clave..." class="form-control" />
+									</div>
+
+							
+								</div>
+								<div class="footer text-center">
+									<a href="#yy" class="btn btn-simple btn-primary btn-lg">Entrar</a>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+      </div>
+      
+    </div>
+  </div>
+	</div>
